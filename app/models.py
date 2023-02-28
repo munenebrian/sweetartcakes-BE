2f093fdd5526fffbb8f5feb35428b32e58784166
Job Karani <jobkarani084j@gmail.com>
2023-02-28 15:20:42 +0300

    feat: separates wedding cakes from occassional cakes

--- app/models.py
+++ app/models.py
@@ -17,7 +17,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class Product(models.Model):
+class OccassionalCakes(models.Model):
     name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     image = ImageField( manual_crop="")
@@ -28,6 +28,23 @@ class Product(models.Model):
     old_price = models.FloatField()
     is_available = models.BooleanField(default = True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+    
+class WeddingCakes(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = ImageField( manual_crop="")
+    image2 = ImageField(blank=True, null=True, manual_crop="")
+    image3 = ImageField(blank=True,null=True, manual_crop="")
+    description = models.TextField(max_length=4000)
+    new_price = models.FloatField()
+    old_price = models.FloatField()
+    is_available = models.BooleanField(default = True)
 
     class Meta:
         ordering = ('name',)
@@ -54,19 +71,9 @@ class Blogs(models.Model):
     heading = models.CharField(max_length=100, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     text = models.TextField(max_length=1000, blank=False, null=True)
-    tex2 = models.TextField(max_length=1000, blank=True, null=True, default="")
-    text3 = models.TextField(max_length=1000, blank=True, null=True, default="")
-    blog_category = models.ForeignKey(BlogCategory, on_delete=models.CASCADE, default="")
+    tex2 = models.TextField(max_length=1000, blank=True, null=True)
+    text3 = models.TextField(max_length=1000, blank=True, null=True)
+    blog_category = models.ForeignKey(BlogCategory, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.heading
-
-class Service(models.Model):
-    image = ImageField( manual_crop="")
-    name = models.CharField(max_length=100, blank=False)
-    paragrapagh1 = models.TextField(max_length=1000, blank=False, null=True)
-    paragrapagh2 = models.TextField(max_length=1000, blank=True, null=True)
-    paragrapagh3 = models.TextField(max_length=1000, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
